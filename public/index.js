69b6cb12928877a152d93ed38a8bd2da2a4280db
EnshaednHiker <nbingham09@gmail.com>
2017-10-10 17:16:14 -0500

    cleaned up straggling file paths in index.html

--- public/index.js
+++ public/index.js
@@ -5,6 +5,13 @@ import about from './about';
 import account from './account';
 import components from './components';
 import css from './index.css'
+import bootstrapSocialCSS from '~/assets/styles/bootstrap-social.css';
+import fontAwesome from '~/assets/styles/font-awesome.min.css';
+import backgroundPicXS from '~/assets/sleeping-city-sleep-night_extraSmall.jpeg'
+import backgroundPicSm from '~/assets/sleeping-city-sleep-night_small.jpeg'
+import backgroundPicMed from '~/assets/sleeping-city-sleep-night_medium.jpeg'
+import backgroundPicLg from '~/assets/sleeping-city-sleep-night_large.jpeg'
+import backgroundPicXL from '~/assets/sleeping-city-sleep-night_extraLarge.jpeg'
 
 const routes = {
 
