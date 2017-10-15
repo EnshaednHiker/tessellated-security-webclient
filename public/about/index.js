f4494ddb0ef15d50b97977e4ef30fd4ddf8ce701
EnshaednHiker <nbingham09@gmail.com>
2017-10-15 17:41:06 -0500

    updated about page to be more cohesive, added accordion buttons to encapsulate tutorial steps

--- public/about/index.js
+++ public/about/index.js
@@ -4,4 +4,18 @@ import $ from 'jquery'
 
 export default function () {
     $('#wrapper').html(view);
+    let acc = document.getElementsByClassName("accordion");
+    let i;
+    
+    for (i = 0; i < acc.length; i++) {
+        acc[i].onclick = function() {
+          this.classList.toggle("active");
+          var panel = this.nextElementSibling;
+          if (panel.style.maxHeight){
+            panel.style.maxHeight = null;
+          } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+          } 
+        }
+      }
 }
